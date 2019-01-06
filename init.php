afacb83459bdb133ab2aeb7bf11730a09508839b
Roel Philipsen <rgmphilipsen@gmail.com>
2019-01-06 11:31:54 +0100

    Dedouble sentences

--- init.php
+++ init.php
@@ -1,9 +1,11 @@
 <?php
 $chapters = getChapters();
+$existingSentences = [];
 function getChapters() {
     $response = file_get_contents('https://docs.google.com/spreadsheets/d/e/2PACX-1vS2k-Y_zTl8sss7-w5cOBaAh4jTNP7q_vwQk-IvIyXEtv134RKXQkvs3izOiwmYiBSILc1aB8ySL-1p/pub?gid=0&single=true&output=csv');
     $rows = explode("\n", $response);
     $chapters = [];
+    $existingSentences = [];
     foreach ($rows as $index => $row) {
         if ($index === 0) {
             continue;
@@ -17,11 +19,16 @@ function getChapters() {
             ];
         }
 
-        $sentence = [
-            'nl' => trim($columns[2]),
-            'de' => trim($columns[3]),
-        ];
-        $chapters[count($chapters)-1]['sentences'][] = $sentence;
+        $sentenceDutch = trim($columns[2]);
+        $sentenceGerman = trim($columns[3]);
+        if (!in_array($sentenceDutch, $existingSentences)) {
+            $existingSentences[] = $sentenceDutch;
+            $sentence = [
+                'nl' => $sentenceDutch,
+                'de' => $sentenceGerman,
+            ];
+            $chapters[count($chapters)-1]['sentences'][] = $sentence;
+        }
     }
 
     foreach ($chapters as &$chapter) {
